11f0b7069a6cfb9b016f6f56ea3af1be9ff0fc60
Boaz Kaufman <bk1nyc@gmail.com>
2026-01-10 22:12:34 -0500

    Styling; add lorem ipsum

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,20 +1,23 @@
 import ButtonLink from '../ButtonLink/ButtonLink';
 import './Home.css';
-import { Link } from "react-router"
+import { loremIpsum } from '../../constants';
 
 export default function Home() {
   return (
-    <div>
+    <div className="slide_up">
       <h1>Team Best Privacy Toolbox</h1>
-      <p className="slide_up">
+      <p>
         This website provides resources for anyone looking to improve their personal security and privacy posture.
+      </p>
+      <p>
+        {loremIpsum}
       </p>
       <div className="home-links">
         <ButtonLink to="KnowledgeHub">
-          View our knowledge hub.
+          View our knowledge hub
         </ButtonLink>
         <ButtonLink to="Downloads">
-          View our privacy tool downloads.
+          View our privacy tool downloads
         </ButtonLink>
       </div>
     </div>
