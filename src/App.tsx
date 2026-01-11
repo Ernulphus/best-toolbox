6d5a5e8260d966d1c9335f69f3f754014f54a1a5
Boaz Kaufman <bk1nyc@gmail.com>
2026-01-11 18:03:00 -0500

    Add routes for hub and downloads

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/KnowledgeHub" />
+        <Route path="/Downloads" /> 
       </Routes>
     </BrowserRouter>
   );
